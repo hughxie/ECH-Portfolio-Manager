0d2c6feadf3cc7f890f80b311a573e01b6bb79e5
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-11-04 06:10:10 -0400

    Backend server for retrieving stock prices and saving them to firebase database

--- ticker_backend/StockAPI.js
+++ ticker_backend/StockAPI.js
@@ -1,11 +1,30 @@
 var https = require('https'); //need to http
 var http = require('http');
-var stocks = ['CYBR', 'SHOP', 'IBM', 'AAPL', 'ANF', 'TWX', 'T', 'SNAP', 'P','AMZN','AMD','PYPL','UA','WFC','JCP','M','MRO','BAC','GOOG'];
+var admin = require("firebase-admin");
+var serviceAccount = require("ECH-Portfolio-Manager-989fb0879c82.json");
+var arrayOfInfo = [];
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://ech-portfolio-manager.firebaseio.com/"
+});
+var db = admin.database();
+var ref = db.ref("/");
+
+
+var stocks = ['CYBR', 'SHOP', 'IBM', 'AAPL', 'ANF', 'TWX', 'T', 'SNAP', 'P','AMZN','AMD','PYPL','UA','WFC','JCP','M','MRO','BAC','GOOG','NFLX','NFLX'];
+
+getStockData('INTRADAY',stocks[0],1,0);
+//timer = setInterval(middleFunc, 60000);
 http.createServer(function (request,response){
 
-	getStockData('INTRADAY',stocks[0],1,0);
-	timer = setInterval(middleFunc, 60000);
+
 }).listen(3000);
+
+function middleFunc(){
+	arrayOfInfo = [];
+	getStockData('INTRADAY',stocks[0],1,0);
+
+}
 function getStockData(timeRange,symb,interval, p){
 	//timeRange is either 'INTRADAY', 'DAILY', 'DAILY_ADJUSTED', 'WEEKLY', 'WEEKLY_ADJUSTED', 'MONTHLY', 'MONTHLY_ADJUSTED'
 	//interval is either 1,5,15,30,60
@@ -33,15 +52,34 @@ function getStockData(timeRange,symb,interval, p){
 			var obj = JSON.parse(content);
 
 			if(obj != null) {
-				var currentPrice = obj[messageKey][Object.keys(obj[messageKey])[0]]['1. open'];
+				var message =  obj[messageKey][Object.keys(obj[messageKey])[0]];
+				var currentPrice = message['1. open'];
+				var currentHigh = message['2. high'];
+				var currentLow = message['3. low'];
+				var currentClose = message['4. close'];
+				var currentVolume = message['5. volume'];
 				console.log(symb + ' is currently ' + currentPrice);
-				p++;
-				if(p == stocks.length){
+
+				if(p == stocks.length-1){
 					console.log('DONE');
+					for(var y = 0; y < stocks.length-1; y++){
+						retrieveNameFromSymbol(stocks[y], y);
+
+					}
+					console.log('done');
 				}
 				else {
-				getStockData('INTRADAY',stocks[p],1, p);
-			}
+					p++;
+					var tempObj = {}
+					tempObj.symb = symb;
+					tempObj.price = currentPrice;
+					tempObj.high = currentHigh;
+					tempObj.low = currentLow;
+					tempObj.close = currentClose;
+					tempObj.volume = currentVolume;
+					arrayOfInfo.push(tempObj);
+					getStockData('INTRADAY',stocks[p],1, p);
+				}
 			}
 		})
 
@@ -50,4 +88,41 @@ function getStockData(timeRange,symb,interval, p){
 		console.log("Error finding matches! User may not exist or there is a problem with the connection. Returning error to the client");
 	});
 	req.end();
+}
+
+function retrieveNameFromSymbol(symb, y){
+	var options = {
+		host: 'finance.yahoo.com',
+		port: 443,
+		path: '/quote/' + symb
+	};
+	var content = "";
+	https.get(options, function(res) {
+		res.on("data", function (chunk) {
+			content += chunk;
+
+		})
+		res.on("end",function (){
+			var tempArray0 = content.split("<h1 class=\"D(ib) Fz(18px)\" data-reactid=\"7\">");
+			var tempArray = tempArray0[1].split("(");
+			var name = tempArray[0];
+			writeToServer(symb,arrayOfInfo[y],name);
+		})
+
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+		console.log("Error! User may not exist or there is a problem with the connection. Returning error to the client");
+	});
+}
+
+function writeToServer(symb, currentStock, name){
+	var symbolsRef = ref.child("symbols/" + symb);
+	symbolsRef.set({
+		name: name,
+		price: currentStock.price,
+		high: currentStock.high,
+		low: currentStock.low,
+		close: currentStock.close,
+		volume: currentStock.volume
+	});
 }
