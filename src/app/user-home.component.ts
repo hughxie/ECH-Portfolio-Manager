a9e9eb57cc24d2896aa2726a4ba38cd363501992
Zach Hauser <zach.whatelse@gmail.com>
2017-11-04 14:05:05 -0400

    Some CSS changes

--- src/app/user-home.component.ts
+++ src/app/user-home.component.ts
@@ -7,9 +7,9 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 import { Stock } from './stock';
 
 const STOCKS: Stock[] = [
-  {id: 1, fullName: 'Google', symbol: 'GOOG', currentPrice: 625, netPercent: 2.34, netChange: 7.95},
+  {id: 1, fullName: 'Google', symbol: 'GOOG', currentPrice: 625.67, netPercent: 2.34, netChange: 7.95},
   {id: 2, fullName: 'Apple', symbol: 'AAPL', currentPrice: 123, netPercent: 1.33, netChange: 6.51},
-  {id: 3, fullName: 'Shopify', symbol: 'SHOP', currentPrice: 93, netPercent: 4.6, netChange: 9.3},
+  {id: 3, fullName: 'Shopify', symbol: 'SHOP', currentPrice: 93.32, netPercent: 4.6, netChange: 9.3},
   {id: 4, fullName: 'Royal Bank of Canada', symbol: 'RBC', currentPrice: 94, netPercent: 1.71, netChange: 0.98},
   {id: 5, fullName: 'Bank of Montreal', symbol: 'BMO', currentPrice: 52, netPercent: 3.64, netChange: 8.42}
 ];
